dce9e1e2fbb1ddb7098e6e8645447132d0bfa0d2
tooBusyNow <OldsmobileBuick54@yandex.ru>
2023-03-19 20:51:04 +0500

    Pipelines now could create unique environment for every branch: auto-creation & configuration of Compute instances; ability to deploy latest code on particular machines. So, this is some basic implementation of GitOps paradigm.

--- yc-terraform-testserver/testserver.tf
+++ yc-terraform-testserver/testserver.tf
@@ -13,9 +13,14 @@ terraform {
 
 # Using Yandex Terraform provider
 provider "yandex" {
-  zone      = "ru-central1-a"
-  cloud_id  = "b1g5iekm4kbh4enphdu6"
-  folder_id = "b1g8vp06auhfbk8j09np"
+  zone      = var.zone
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+}
+
+terraform {
+  backend "http" {
+  }
 }
 
 # Latest Ubuntu 22.04 image
@@ -25,29 +30,27 @@ data "yandex_compute_image" "ubuntu-2204-latest" {
 
 # Using the same subnet as specified in our zone
 data "yandex_vpc_subnet" "default-subnet-a" {
-  name = "default-ru-central1-a"
+  name = var.subnet
 }
 
-# Creating public static IP for this TestVM instance
-resource "yandex_vpc_address" "remote-public-addr" {
-  name = "static ip for test-vm"
+# Creating public static IP for this VM instance
+resource "yandex_vpc_address" "public-addr" {
+  name = var.address_name
 
   external_ipv4_address {
-    zone_id = "ru-central1-a"
+    zone_id = var.zone
   }
 }
 
-# Creating Test Server instance
-resource "yandex_compute_instance" "test-vm-instance" {
+# Creating VM Instance
+resource "yandex_compute_instance" "vm-instance" {
 
   depends_on = [
-    yandex_vpc_address.remote-public-addr
+    yandex_vpc_address.public-addr
   ]
 
-  name = "test-instance"
-
-  # Intel Ice Lake
-  platform_id = "standard-v3"
+  name        = var.vm_name
+  platform_id = var.platform_id
 
   # Resources of this instance
   resources {
@@ -60,17 +63,17 @@ resource "yandex_compute_instance" "test-vm-instance" {
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu-2204-latest.id
-      size     = 25
+      size     = var.boot_disk_size
     }
   }
 
   # Creating default network interface with public static IPv4 address
   network_interface {
-    subnet_id      = data.yandex_vpc_subnet.default-subnet-a.subnet_id
-    ipv6           = false
+    subnet_id = data.yandex_vpc_subnet.default-subnet-a.subnet_id
+    ipv6      = false
 
     nat            = true
-    nat_ip_address = yandex_vpc_address.remote-public-addr.external_ipv4_address[0].address
+    nat_ip_address = yandex_vpc_address.public-addr.external_ipv4_address[0].address
   }
 
   # Using metadata for ssh connection management
@@ -80,20 +83,20 @@ resource "yandex_compute_instance" "test-vm-instance" {
 
   # Sets up connection that is used for remote configuration
   connection {
-    type = "ssh"
-    user = "bezdna-remote"
-    private_key = "${file("~/.ssh/id_rsa")}"
-    host = "${self.network_interface.0.nat_ip_address}"
+    type        = "ssh"
+    user        = "gitlab"
+    private_key = file("~/gitlab_id_rsa")
+    host        = self.network_interface.0.nat_ip_address
   }
 
   # And this Provisioner starts instance configuration script
   provisioner "remote-exec" {
-      when = create
-      script = "../shell/terraform/start-testserver-config.sh"
+    when   = create
+    script = "../shell/terraform/start-testserver-config.sh"
   }
 }
 
 # Prints Public IP of created instance
 output "connection_ip" {
-  value = yandex_compute_instance.test-vm-instance.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.vm-instance.network_interface.0.nat_ip_address
 }
