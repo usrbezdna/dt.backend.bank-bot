8fe1cdfaef1770566802740b5b697c512aa72122
tooBusyNow <OldsmobileBuick54@yandex.ru>
2023-04-12 02:30:36 +0500

    Added Django debug toolbar

--- src/config/urls.py
+++ src/config/urls.py
@@ -6,4 +6,11 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("app.internal.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += path('__debug__/', include('debug_toolbar.urls')), 
+
+urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
