30d81ba90b461a553b84a3f264f7f72b37324ce5
tooBusyNow <OldsmobileBuick54@yandex.ru>
2023-05-07 22:02:58 +0500

    Models were separated between api entities

---
+++ src/app/internal/api_v1/accounts/db/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from app.internal.api_v1.users.db.models import User
+
+class Account(models.Model):
+    """
+    Account model.
+
+    """
+
+    PERSON_CHOICES = [
+        ("PER", "Person"),
+        ("ENT", "Entity"),
+    ]
+
+    CURRENCY_CHOICES = [
+        ("USD", "United States dollar"),
+        ("EUR", "Euro"),
+        ("GBP", "British pound"),
+        ("TRY", "Turkish lira"),
+        ("RUB", "Russian ruble"),
+    ]
+
+    uniq_id = models.CharField(primary_key=True, max_length=40)
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    party = models.CharField(max_length=3, choices=PERSON_CHOICES)
+
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+    value = models.DecimalField(max_digits=19, decimal_places=2)
+
+    class Meta:
+        """
+        Account metadata
+        """
+
+        verbose_name = "Account"
+        db_table = "payment_accounts"
---
+++ src/app/internal/api_v1/cards/db/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from app.internal.api_v1.accounts.db.models import Account
+
+class Card(models.Model):
+    """
+    Card model.
+
+    """
+
+    uniq_id = models.CharField(primary_key=True, max_length=20)
+    corresponding_account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    expiration = models.DateField()
+
+    class Meta:
+        """
+        Card metadata
+        """
+
+        verbose_name = "Card"
+        db_table = "payment_cards"
